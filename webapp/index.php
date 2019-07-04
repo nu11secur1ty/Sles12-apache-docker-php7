fd02aaac3f077c82da955df34adef8e4fc1c175d
nu11secur1ty <venvaropt@gmail.com>
2019-07-04 17:38:04 +0300

    Update index.php

--- webapp/index.php
+++ webapp/index.php
@@ -1,3 +1,12 @@
+
 <?php
-	phpinfo();
+
+// Show all information, defaults to INFO_ALL
+phpinfo();
+
+// Show just the module information.
+// phpinfo(8) yields identical results.
+phpinfo(INFO_MODULES);
+
 ?>
+
